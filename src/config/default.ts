6cd4f53ed9cd6db0a3476bbf77a217d78367aec7
Samuel Ngwarai <samuel.ngwarai@free2move.com>
2023-11-17 22:57:44 +0100

    fix: update repo packages and node version

--- src/config/default.ts
+++ src/config/default.ts
@@ -3,7 +3,7 @@ import { IConfigObject } from './config-i';
 const defaultConfig: IConfigObject = {
   PORT: 3001,
   LOG_LEVEL: 'info',
-  CREATE_SWAGGER_FILE: false
+  CREATE_SWAGGER_FILE: true
 };
 
 export = defaultConfig;
